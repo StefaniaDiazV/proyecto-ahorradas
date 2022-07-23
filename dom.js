4dd48883c46a3abca6a2576c20d36c89e903ddd4
Stefania Diaz <stefaniadiazvalencia@gmail.com>
2022-07-23 12:34:08 -0500

    Update/Funciones:select,mostrar operaciones

--- dom.js
+++ dom.js
@@ -43,3 +43,79 @@ btnReportes.addEventListener("click", () => {
   nuevaOperacion.classList.add("d-none");
   vistaCategorias.classList.add("d-none");
 });
+
+// Función agregar categorias-select
+
+const categorias = [
+  'Comida',
+  'Servicios',
+  'Salidas',
+  'Transporte',
+  'Educación',
+  'Trabajo'
+];
+
+const generarCategorias = () => {
+  const selects = document.getElementsByClassName('categorias-select');
+  for(let i = 0; i < selects.length; i++){
+    const select = selects[i];
+    if(select.classList.contains('filtro-categoria')){
+      select.innerHTML = '<option>Todas</option>'
+    }
+    for(let j = 0; j < categorias.length; j++){
+      select.innerHTML += `<option value=${categorias[j]}>${categorias[j]}</option>`
+    }
+  }
+}
+
+generarCategorias();
+
+
+// Funcion Mostrar tilulos-Operaciones
+
+const operaciones = [];
+
+const mostraroperaciones = (arr) => {
+  if(!arr.length){
+    document.getElementById('sin-operaciones').classList.remove('d-none')
+    document.getElementById('con-operaciones').classList.add('d-none')
+  } else { 
+    document.getElementById('sin-operaciones').classList.add('d-none')
+    document.getElementById('con-operaciones').classList.remove('d-none')
+  }
+} 
+
+mostraroperaciones(operaciones);
+
+// Funcion llenar operaciones 
+
+const descripcionOperacion = document.getElementById('descripcion-operacion'); // input-descripcion vista nueva operacion
+const montoOperacion = document.getElementById('monto-operacion'); // input-monto 
+const tipoOperacion = document.getElementById('tipo-operacion'); // select-tipo de operacion
+const categoriaNuevaOperacion = document.getElementById('categoria-nueva-operacion');
+const fechaOperacion = document.getElementById('fecha-operacion');
+const btnAgregarOperacion = document.getElementById('btn-agregar-operacion');
+
+
+descripcionOperacion.addEventListener('change', () => {
+  console.log(descripcionOperacion.value)
+})
+
+
+const crearObjOperaciones = () => {
+ 
+};
+
+
+btnAgregarOperacion.addEventListener('click', () => {
+  obj = {
+    descripcion:descripcionOperacion.value,
+    monto:montoOperacion.value,
+    tipo:tipoOperacion.value,
+    categoria:categoriaNuevaOperacion.value,
+    fecha: fechaOperacion.value
+  }
+  operaciones.push(obj)
+})
+
+console.log(operaciones)
