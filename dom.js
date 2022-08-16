a46a3048a13f3e4118f85f14f2841c692a5c17e3
Stefania Diaz <stefaniadiazvalencia@gmail.com>
2022-08-16 12:47:57 -0500

    Update/Agregando funcion ocultar filtros

--- dom.js
+++ dom.js
@@ -855,6 +855,29 @@ const pintarMayorGastoPorMes = (arr) => {
 
 totalPorMes(operaciones);
 
+
+// FUNCION OCULTAR Y MOSTRAR FILTROS
+
+const contenedorFiltros = document.getElementById('contenedor-filtros');
+const linkOcultarFiltros = document.getElementById('link-ocultar-filtros');
+const linlMostrarFiltros = document.getElementById('link-mostrar-filtros');
+
+
+linkOcultarFiltros.addEventListener('click', () => {
+    contenedorFiltros.classList.add('d-none');
+    linlMostrarFiltros.classList.remove('d-none');
+    linkOcultarFiltros.classList.add('d-none');
+});
+
+linlMostrarFiltros.addEventListener('click', () => {
+  contenedorFiltros.classList.remove('d-none');
+  linlMostrarFiltros.classList.add('d-none');
+  linkOcultarFiltros.classList.remove('d-none');
+});
+
+
+
+
 const inicializar = () => {
   const inputsFecha = document.querySelectorAll('input[type="date"]');
   inputsFecha.forEach((input) => {
